cb8d3bd9a3fa21e784c663660ba87bf3cf0e033c
Redenied <chavasalasb@hotmail.com>
2024-03-27 10:41:20 -0600

    Split functionality into guessing(word, show) and main() functions.
    -guessing function takes word and show parameters.
    -main function generates the word chosen randomly and calls for guessing.
    -Deleted the answer variable to use instead a while True loop, breaking once the user has guessed the whole word.
    -Commented code for clarity.

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -15,21 +15,30 @@ words = [
 "florida"
 ]
 
-word = words[random.randrange(len(words))]
-answer = False
-show = ["_" for _ in range(len(word))]
+def guessing(word, show):
+	while True:
+		guess = input("Guess a character: ")
+		# Conert '_' into the guessed letter if its in word
+		for i in range(len(word)):
+			if word[i] == guess:
+				show[i] = guess
+		for char in show:
+			print(char)
+		# If we have no hidden letters, it means the word is discovered
+		if "_" not in show:
+			break
+	return
 
-for char in show:
-	print(char)
+def main():
+	word = words[random.randrange(len(words))]	# Get word picking a random list index
+	show = ["_" for _ in range(len(word))]		# List of underscores the size of word
 
-while not answer:
-	guess = input("Guess a character: ")
-	for i in range(len(word)):
-		if word[i] == guess:
-			show[i] = guess
-	for char in show:
+	for char in show:	# Print vertically the hidden letters
 		print(char)
 
-	if "_" not in show:
-		break
-print(f"Correct! The answer is {word}")
+	guessing(word, show)
+
+	print(f"Correct! The answer is {word}")
+	return
+
+main()
