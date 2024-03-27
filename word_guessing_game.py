a5e06da3839042e4956efb643822e646e1726a00
Redenied <chavasalasb@hotmail.com>
2024-03-27 13:22:03 -0600

    Added play again feature and added more comments.
    Fixed comment with a typo, added a play again feature in main() function that erases first the word and show variables.

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -18,10 +18,14 @@ words = [
 def guessing(word, show):
 	while True:
 		guess = input("Guess a character: ")
-		# Conert '_' into the guessed letter if its in word
+		# Convert '_' into the guessed letter if its in word
 		for i in range(len(word)):
 			if word[i] == guess:
 				show[i] = guess
+		if (guess not in word and len(guess) == 1) or len(guess)>1:
+			print("Whoops! Try again.")
+
+		# Print underscores with revealed letters
 		for char in show:
 			print(char)
 		# If we have no hidden letters, it means the word is discovered
@@ -39,6 +43,16 @@ def main():
 	guessing(word, show)
 
 	print(f"Correct! The answer is {word}")
+
+	# Play again if user answers yes
+	play_again = input("Want to play again?(Y/N) ").lower()
+	if play_again == "y":
+		# Clear word and show and run main again
+		word = ""
+		show = []
+		main()
+
 	return
 
 main()
+
